dae3c7e9ff4b7bc89c46875f3119c19525dbbd6d
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-08-30 16:57:52 -0500

    Add TokenInterface.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Interface/TokenInterface.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\Plex\Interface;
+
+use JsonSerializable;
+use Stringable;
+
+interface TokenInterface extends JsonSerializable, Stringable
+{
+}
